c7d85e2e15829be8c4fe5f5a817ff1106233ab95
Muhamed-Hashesh <muhamed7as4h@gmail.com>
2024-08-05 02:33:33 +0300

    social authentication buttons

--- lib/views/authentication/auth_page_home.dart
+++ lib/views/authentication/auth_page_home.dart
@@ -1,6 +1,7 @@
 import 'package:chit_chat_app/helper/sized_box.dart';
 import 'package:chit_chat_app/views/authentication/widgets/auth_body_container.dart';
 import 'package:chit_chat_app/widgets/app_logo.dart';
+import 'package:chit_chat_app/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -13,7 +14,7 @@ class AuthPageHome extends StatelessWidget {
       backgroundColor: Theme.of(context).colorScheme.surface,
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.only(left: 16, right: 16, top: 16),
           child: SingleChildScrollView(
             child: Column(
               children: [
@@ -23,6 +24,22 @@ class AuthPageHome extends StatelessWidget {
                 ),
                 60.height,
                 const AuthBodyContainer(),
+                32.height,
+                CustomButton(
+                  title: 'Google',
+                  logoSvgAsset: 'assets/icons/google.svg',
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  onTap: () {},
+                  isAuthenticationMethod: true,
+                ),
+                16.height,
+                CustomButton(
+                  title: 'Facebook',
+                  logoSvgAsset: 'assets/icons/facebook.svg',
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  onTap: () {},
+                  isAuthenticationMethod: true,
+                )
               ],
             ),
           ),
