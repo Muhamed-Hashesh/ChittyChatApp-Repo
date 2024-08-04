0a4bc68eafe370be8355df9a70016f06b7ea62f2
Muhamed-Hashesh <muhamed7as4h@gmail.com>
2024-08-04 21:13:54 +0300

    firebase auth declaration

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,41 @@
+import 'dart:developer';
 import 'package:chit_chat_app/config/get_pages_path.dart';
 import 'package:chit_chat_app/config/themes.dart';
+import 'package:chit_chat_app/firebase_options.dart';
 import 'package:chit_chat_app/views/welcome_page/welcome_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        log('========= User is currently signed out! ===========');
+      } else {
+        log('========== User is signed in! ===========');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
