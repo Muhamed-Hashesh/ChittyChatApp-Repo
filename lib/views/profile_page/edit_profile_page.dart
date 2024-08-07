e6108743dcc3bc8e1fa30aa0e6c44ca7989f1547
Muhamed-Hashesh <muhamed7as4h@gmail.com>
2024-08-07 13:59:56 +0300

    handle profile edit image

--- lib/views/profile_page/edit_profile_page.dart
+++ lib/views/profile_page/edit_profile_page.dart
@@ -9,24 +9,52 @@ class EditProfilePage extends StatelessWidget {
     return Scaffold(
       appBar:
           profileAppBar(context, title: 'Edit your profile', hasAction: false),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(24),
-          margin: const EdgeInsets.all(24),
-          decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.primaryContainer,
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('assets/images/girl.png', width: 120),
-                ],
-              ),
-            ],
-          ),
+      body: Container(
+        padding: const EdgeInsets.all(24),
+        margin: const EdgeInsets.all(24),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.primaryContainer,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Stack(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ClipOval(
+                        child: Image.asset(
+                          'assets/images/girl.png', // Your avatar image
+                          fit: BoxFit.cover,
+                          width: 150,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 15,
+                      right: 0,
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.blue,
+                          shape: BoxShape.circle,
+                        ),
+                        padding: const EdgeInsets.all(8),
+                        child: const Icon(
+                          Icons.camera_alt,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
